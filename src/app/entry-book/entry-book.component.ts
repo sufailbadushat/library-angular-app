7079d159ec5ae5c8c18eb8c133e0d4869d082eee
sufailbadushat <sufailbadushat@gamil.com>
2022-11-25 00:09:09 +0530

    add book create

---
+++ src/app/entry-book/entry-book.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-book',
+  templateUrl: './entry-book.component.html',
+  styleUrls: ['./entry-book.component.css']
+})
+export class EntryBookComponent {
+
+}
